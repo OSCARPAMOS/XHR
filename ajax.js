672a703f735b4ae1ce0170f2305bdff0859abb2e
Oscar Pamos <106927863+OSCARPAMOS@users.noreply.github.com>
2022-08-10 06:23:24 +0200

    ej AJAX1

---
+++ ajax.js
@@ -0,0 +1,25 @@
+
+
+const ajax1 = new XMLHttpRequest();
+ $insertar = document.getElementById("insertar");
+ $fragmento = document.createDocumentFragment();
+
+ajax1.addEventListener("readystatechange", (e) => {
+
+      let salidajson = JSON.parse (ajax1.responseText);
+
+      salidajson.forEach( el => {
+        const $li = document.createElement("li");
+        $li.innerHTML = el.id;
+        $fragmento.appendChild($li);
+
+
+      })
+
+      $insertar.appendChild($fragmento);
+
+});
+
+ajax1.open ("GET", "https://jsonplaceholder.typicode.com/posts");
+
+ajax1.send();
